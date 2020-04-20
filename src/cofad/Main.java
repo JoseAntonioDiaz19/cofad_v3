6972d96a49a8ee6954339b2b8c5158729184b02e
tapac <tapac@DESKTOP-KJIUFB1>
2020-04-19 21:25:15 -0500

    Modulo perfil: Experiencia docente actualizado

--- src/cofad/Main.java
+++ src/cofad/Main.java
@@ -6,6 +6,7 @@ import controllers.login.LoginController;
 public class Main {
 
     public static void main(String[] args) {
+        //Ejecucion principal
         Login vista = new Login();
         LoginController ctrl = new LoginController(vista);
     }   
